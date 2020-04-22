6f884d547d6a897367c570a01204900985f9726d
WayneHan670102 <wayne670102@gmail.com>
2020-04-22 23:56:02 +0800

    add new line 2

---
+++ index.js
@@ -0,0 +1,13 @@
+var startServer=require("./server");
+var route=require("./route");
+var requsestHandler=require("./requestHandlers");
+
+var handle={}
+handle['/']=requsestHandler.down;
+handle['/aaa']=requsestHandler.down;
+handle['/bbb']=requsestHandler.upload;
+startServer.start(route.route,handle);
+
+//new line
+//new line 2
+//startServer.start();
